2294b7066bd3b7f5510d5159a5b226ec12a64739
AlexanderGonchar <goncharas24@gmail.com>
2022-09-18 00:14:14 +0300

    add z6

---
+++ z6/Program.cs
@@ -0,0 +1,15 @@
+﻿int Any(int num1, int num2, int pos)
+{
+    if (pos <= 1) return num1 + num2;
+    return Any(num1, num2, pos - 1) + Any(num1, num2, pos - 2);
+}
+Console.WriteLine("Первая цифра?");
+int first = int.Parse(Console.ReadLine());
+Console.WriteLine("Вторая цифра?");
+int second = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите колличество цифр?");
+int size = int.Parse(Console.ReadLine());
+for (int i = 1; i <= size; i++)
+{
+    Console.Write($"{Any(first, second, i)} ");
+}
