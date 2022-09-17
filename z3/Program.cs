8b1c0c790ba4b138c4fc08c75211cdcd92b4042e
AlexanderGonchar <goncharas24@gmail.com>
2022-09-18 00:13:35 +0300

    add z3

---
+++ z3/Program.cs
@@ -0,0 +1,10 @@
+﻿int SumDigits(int number)
+{
+    if (number < 1) return 0;
+    int result = 0;
+    return SumDigits(number / 10) + number % 10;
+}
+Console.Write("int number: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"Sum digits: {SumDigits(number)}");
+
