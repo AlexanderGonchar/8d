cee0cc4e5175b2fe86e575bfe3821da655c3c9c5
AlexanderGonchar <goncharas24@gmail.com>
2022-09-18 00:13:59 +0300

    add z5

---
+++ z5/Program.cs
@@ -0,0 +1,13 @@
+﻿int PowAB(int a, int b)
+{
+    if (b == 0) return 1;
+    else return PowAB(a, b - 1) * a;
+}
+
+Console.Write("Введите число А: ");
+int a = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите число B: ");
+int b = int.Parse(Console.ReadLine() ?? "");
+
+Console.WriteLine($"Число {a} в степени {b} равно {PowAB(a, b)}");
