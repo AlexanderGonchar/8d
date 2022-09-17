5005ab4db207a546c0f0a387098c7ba4d9a6d5ff
AlexanderGonchar <goncharas24@gmail.com>
2022-09-18 00:12:49 +0300

    add z1

---
+++ z1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите числа N и M, N>M");
+Console.WriteLine("N = ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("M = ");
+int m = int.Parse(Console.ReadLine());
+void Recursion(int n)
+{
+    if (n < m) return;
+    {
+        Recursion(n - 1);
+        Console.Write(n + " ");
+    }
+}
+Recursion(n);
